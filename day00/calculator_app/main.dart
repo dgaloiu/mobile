c6176d77a25b93bcf0fc226293ba5a11af6c8e94
dgaloiu <danpaulgaloiu@gmail.com>
2025-04-24 20:21:56 +0200

    operations priority

--- day00/calculator_app/main.dart
+++ day00/calculator_app/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController inputController = TextEditingController();
   final TextEditingController resultController = TextEditingController();
+  bool showGochaImage = false;
 
   final List<String> buttonLabels = [
     '7', '8', '9', 'C', 'AC',
@@ -86,26 +88,77 @@ class _MyHomePageState extends State<MyHomePage> {
           List parts = expression.split(RegExp(r'(\+|\-|\*|\/)'));
           String operators = expression.replaceAll(RegExp(r'[0-9]'), '');
           String result = '';
-          for (int i = 0; i < parts.length; i++) {
-            if (i == 0) {
-              result = parts[i];
-            } else {
+          String firstPart = parts[0];
+
+          if(parts.length != 1){
+            for (int i = 1; i < parts.length; i++) {
+
               String operator = operators[i - 1];
-              String nextPart = parts[i];
-              if (operator == '+') {
-                result = (double.parse(result) + double.parse(nextPart)).toString();
-              } else if (operator == '-') {
-                result = (double.parse(result) - double.parse(nextPart)).toString();
-              } else if (operator == '*') {
-                result = (double.parse(result) * double.parse(nextPart)).toString();
-              } else if (operator == '/') {
-                result = (double.parse(result) / double.parse(nextPart)).toString();
+              String secondPart = parts[i];
+              if (operator == '+' || operator == '-'){
+                if (i == parts.length - 1) {
+                  result += double.parse(firstPart).toString()+operator+double.parse(secondPart).toString();
+                }
+                else {
+                  result += double.parse(firstPart).toString() + operator;
+                }
+                firstPart = secondPart;
+              }
+              else if (operator == '*') {
+                String tmp= (double.parse(firstPart) * double.parse(secondPart)).toString();
+                firstPart = tmp;
+                if (i == parts.length - 1) {
+                  result += double.parse(firstPart).toString();
+                }
+              }
+              else if (operator == '/') {
+                String tmp= (double.parse(firstPart) / double.parse(secondPart)).toString();
+                firstPart = tmp;
+                if (i == parts.length - 1) {
+                  result += double.parse(firstPart).toString();
+                }
               }
             }
           }
-          //make easter egg for division by zero instead of infinity
+          else {
+            result = firstPart;
+          }
+
+          parts = result.split(RegExp(r'(\+|\-)'));
+          operators = result.replaceAll(RegExp(r'[0-9]'), '');
+          result = parts[0];
+          if(parts.length != 1){
+            result = parts[0];
+            for (int i = 1; i < parts.length;i++){
+              String operator = operators[i - 1];
+              if (operator == '+'){
+                result = (double.parse(result) + double.parse(parts[i])).toString();
+              }
+              else if (operator == '-'){
+                result = (double.parse(result) - double.parse(parts[i])).toString();
+              }
+            }
+          }
+
+          if (result.contains('.') && result.substring(result.indexOf('.')) == '.0') {
+            result = result.substring(0, result.indexOf('.'));
+          }
+          else if (result=="Infinity") {
+            setState(() {
+              showGochaImage = true;
+            });
+            Timer(Duration(seconds: 1), () {
+              setState(() {
+                showGochaImage = false;
+              });
+            });
+            inputController.text = '0';
+            result = '0';
+            print("You really thought you could divide by zero? Gocha!");
+          }
           resultController.text = result;
-        } catch (e) {
+        }
+        catch (e) {
           resultController.text = 'Error';
         }
       }
@@ -166,117 +219,129 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  left: BorderSide(color: goldColor, width: 4.0),
-                  right: BorderSide(color: goldColor, width: 4.0),
-                  bottom: BorderSide(color: goldColor, width: 4.0),
-                ),
-                image: const DecorationImage(
-                  image: AssetImage('assets/images/temple.png'),
-                  fit: BoxFit.fill,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Flexible(
-                      child: TextField(
-                        controller: inputController,
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          fontSize: fontSize,
-                          color: goldColor,
-                          shadows: const [Shadow(color: Colors.black, offset: Offset(1, 1))],
-                        ),
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
+          Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: goldColor, width: 4.0),
+                      right: BorderSide(color: goldColor, width: 4.0),
+                      bottom: BorderSide(color: goldColor, width: 4.0),
                     ),
-                    Flexible(
-                      child: TextField(
-                        controller: resultController,
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
-                          fontSize: fontSize,
-                          color: goldColor,
-                          shadows: const [Shadow(color: Colors.black, offset: Offset(1, 1))],
-                        ),
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/temple.png'),
+                      fit: BoxFit.fill,
                     ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          
-          Expanded(
-            flex: 1,
-            child: Container(
-            decoration: BoxDecoration(
-              color:goldColor,
-              border: Border(
-                left: BorderSide(color: goldColor, width: 4.0),
-                right: BorderSide(color: goldColor, width: 4.0),
-                top: BorderSide(color: goldColor, width: 4.0),
-                bottom: BorderSide(color: goldColor, width: 4.0),
-                ),
-              ),
-              width: double.infinity,
-              child: LayoutBuilder(
-                builder: (context, gridConstraints) {
-                  final double cellWidth = gridConstraints.maxWidth / 5;
-                  final double cellHeight = gridConstraints.maxHeight / 4;
-                  final double aspectRatio = cellWidth / cellHeight;
-                  
-                  return GridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 20,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 5,
-                      mainAxisSpacing: 4,
-                      crossAxisSpacing: 4,
-                      childAspectRatio: aspectRatio,
-                    ),
-                    itemBuilder: (context, index) {
-                      return ElevatedButton(
-                        onPressed: () => handleButtonPress(buttonLabels[index]),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          foregroundColor: getButtonColor(buttonLabels[index]),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: FittedBox(
-                          fit: BoxFit.scaleDown,
-                          child: Text(
-                            buttonLabels[index],
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Flexible(
+                          child: TextField(
+                            controller: inputController,
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              fontSize: fontSize,
+                              color: goldColor,
+                              shadows: const [Shadow(color: Colors.black, offset: Offset(1, 1))],
+                            ),
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
                             ),
                           ),
                         ),
+                        Flexible(
+                          child: TextField(
+                            controller: resultController,
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              fontSize: fontSize,
+                              color: goldColor,
+                              shadows: const [Shadow(color: Colors.black, offset: Offset(1, 1))],
+                            ),
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              
+              Expanded(
+                flex: 1,
+                child: Container(
+                decoration: BoxDecoration(
+                  color:goldColor,
+                  border: Border(
+                    left: BorderSide(color: goldColor, width: 4.0),
+                    right: BorderSide(color: goldColor, width: 4.0),
+                    top: BorderSide(color: goldColor, width: 4.0),
+                    bottom: BorderSide(color: goldColor, width: 4.0),
+                    ),
+                  ),
+                  width: double.infinity,
+                  child: LayoutBuilder(
+                    builder: (context, gridConstraints) {
+                      final double cellWidth = gridConstraints.maxWidth / 5;
+                      final double cellHeight = gridConstraints.maxHeight / 4;
+                      final double aspectRatio = cellWidth / cellHeight;
+                      
+                      return GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 20,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 5,
+                          mainAxisSpacing: 4,
+                          crossAxisSpacing: 4,
+                          childAspectRatio: aspectRatio,
+                        ),
+                        itemBuilder: (context, index) {
+                          return ElevatedButton(
+                            onPressed: () => handleButtonPress(buttonLabels[index]),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              foregroundColor: getButtonColor(buttonLabels[index]),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: EdgeInsets.zero,
+                            ),
+                            child: FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                buttonLabels[index],
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       );
                     },
-                  );
-                },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          if (showGochaImage)
+            Center(
+              child: Image.asset(
+                'assets/images/gocha.jpeg',
+                width: 200,
+                height: 200,
               ),
             ),
-          ),
         ],
       ),
     );
